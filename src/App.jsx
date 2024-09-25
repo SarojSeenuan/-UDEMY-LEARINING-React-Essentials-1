e1e11cb78289fdf60069e3e7be0f67131faa4e90
SarojSeenuan <sarojseenuan@gmail.com>
2024-09-26 01:50:33 +0900

    Final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,69 +1,16 @@
-import { CORE_CONCEPTS } from './data.js';
 import Header from './components/Header/Header.jsx';
-import CoreConcept from './components/CoreConcept.jsx';
-import TabButton from './components/TabButton.jsx';
+import CoreConcepts from './components/CoreConcepts.jsx';
+import Examples from './components/Examples.jsx';
 
 function App() {
-    function handleClick(selectedButton) {
-        // selectedButton => 'components', 'jsx', 'props', 'state'
-        console.log(selectedButton);
-    }
-
     return (
-        <div>
+        <>
             <Header />
             <main>
-                <section id="core-concepts">
-                    <h2>Time to get started!</h2>
-                    <ul>
-                        {/* <CoreConcept
-                            title={CORE_CONCEPTS[0].title}
-                            description={CORE_CONCEPTS[0].description}
-                            image={CORE_CONCEPTS[0].image}
-                        />
-                        <CoreConcept
-                            title={CORE_CONCEPTS[1].title}
-                            description={CORE_CONCEPTS[1].description}
-                            image={CORE_CONCEPTS[1].image}
-                        />
-                        <CoreConcept
-                            title={CORE_CONCEPTS[2].title}
-                            description={CORE_CONCEPTS[2].description}
-                            image={CORE_CONCEPTS[2].image}
-                        />
-                        <CoreConcept
-                            title={CORE_CONCEPTS[3].title}
-                            description={CORE_CONCEPTS[3].description}
-                            image={CORE_CONCEPTS[3].image}
-                        /> */}
-                        <CoreConcept {...CORE_CONCEPTS[0]} />
-                        <CoreConcept {...CORE_CONCEPTS[1]} />
-                        <CoreConcept {...CORE_CONCEPTS[2]} />
-                        <CoreConcept {...CORE_CONCEPTS[3]} />
-                    </ul>
-                </section>
-
-                <section id="examples">
-                    <h2>Examples</h2>
-                    <menu>
-                        {/* <TabButton>Components</TabButton> */}
-                        <TabButton onSelect={() => handleClick('componetns')}>
-                            Components
-                        </TabButton>
-                        <TabButton onSelect={() => handleClick('jsx')}>
-                            JSX
-                        </TabButton>
-                        <TabButton onSelect={() => handleClick('props')}>
-                            Props
-                        </TabButton>
-                        <TabButton onSelect={() => handleClick('state')}>
-                            State
-                        </TabButton>
-                    </menu>
-                    Dynamic Content
-                </section>
+                <CoreConcepts />
+                <Examples />
             </main>
-        </div>
+        </>
     );
 }
 
