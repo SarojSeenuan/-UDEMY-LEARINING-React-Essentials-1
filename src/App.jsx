27bb3754c8bb1a6d42f977f3dc672487c32a458f
SarojSeenuan <sarojseenuan@gmail.com>
2024-10-01 11:30:37 +0900

    First commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,7 @@
-import Header from './components/Header/Header.jsx';
-import CoreConcepts from './components/CoreConcepts.jsx';
-import Examples from './components/Examples.jsx';
-
 function App() {
-    return (
-        <>
-            <Header />
-            <main>
-                <CoreConcepts />
-                <Examples />
-            </main>
-        </>
-    );
+  return (
+    <h1>React Investment Calculator</h1>
+  )
 }
 
-export default App;
+export default App
